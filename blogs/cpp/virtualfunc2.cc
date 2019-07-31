6b3b69602137a1fbeb9db374b57a8ab47f242974
labinxu <flowinair@gmail.com>
2019-07-31 15:20:30 +0800

    virtual function 2

--- blogs/cpp/virtualfunc2.cc
+++ blogs/cpp/virtualfunc2.cc
@@ -1,19 +1,16 @@
 #include<iostream>
 using namespace std;
 struct A{
-  virtual void f1(){cout<<"A::f1()"<<endl;}
+  virtual ~A(){cout<<"destructor A"<<endl;}
+  virtual void f1(){cout<<"A::f1"<<endl;}
+
 };
 struct B :public A{
-  virtual void f1(){cout<<"B::f1()"<<endl;}
+  ~B(){cout<<"destructor B"<<endl;}
 };
 
 int main(){
-  A *pA = new B;
-  (*pA).f1();
-  A(*pA).f1();
-  B b;
-  A(b).f1();
-  A *bref=&b;
-  bref->f1();
+  A *pb = new B;
+  delete pb;
   return 0;
 }

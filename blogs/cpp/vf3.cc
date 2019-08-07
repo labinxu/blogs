43d60271517e26f9ef05164339462206cab75e01
labinxu <flowinair@gmail.com>
2019-08-07 18:06:33 +0800

    vf3

--- blogs/cpp/vf3.cc
+++ blogs/cpp/vf3.cc
@@ -1,6 +1,5 @@
 #include <iostream>
 using namespace std;
-namespace v{
 class A{
 public:
   virtual void af1(){cout<<"A::af1"<<endl;}
@@ -9,32 +8,35 @@ public:
 
 class B{
 public:
-  virtual void bf1(){cout<<"B::bf2"<<endl;}
+  virtual void bf1(){cout<<"B::bf1"<<endl;}
   virtual void bf2(){cout<<"B::bf2"<<endl;}
 };
 class C: public A,public B{
-// public:
-//   virtual void bf2(){cout<<"C::bf2"<<endl;}
-
+public:
+  void af1(){cout<<"C::af1"<<endl;}
+  void bf1(){cout<<"C::bf1"<<endl;}
+  virtual void  cf1(){cout<<"C::cf1"<<endl;}
 };
-}
-namespace nv{
-  class A{
-  public:
-    void f1(){cout<<"A::f1"<<endl;}
-  };
 
-  class B{
-  public:
-    void f1(){cout<<"B::f2"<<endl;}
-  };
-  class C: public A,public B{
-  public:
-    using A::f1;
-  };
-}
+// namespace nv{
+//   class A{
+//   public:
+//     void f1(){cout<<"A::f1"<<endl;}
+//   };
+
+//   class B{
+//   public:
+//     void f1(){cout<<"B::f2"<<endl;}
+//   };
+//   class C: public A,public B{
+//   public:
+//     using A::f1;
+//   };
+// }
 
 int main(){
-  v::C *pc = new v::C;
+  C *pc = new C;
+  pc->af1();
+  pc->C::bf1();
   return 0;
 };
